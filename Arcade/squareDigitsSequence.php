da2844da2aa76dcedbdb0c6304c826ae95564039
Diar Selimi <Diarselimi92@gmail.com>
2016-12-23 00:44:03 +0100

    finished the Square Digits Sequence

--- Arcade/squareDigitsSequence.php
+++ Arcade/squareDigitsSequence.php
@@ -1,2 +1,29 @@
 <?php
+echo '<pre>';
+function squareDigitsSequence($a0) {
+    $results = [];
+    $sum = $a0;
+    $last = 0;
+    while(!in_array($sum, $results)){
+        $last = $sum;
+        $results[] = $last;
+        $sum = calcSumPower($last);
+        echo sprintf('%s <br>',$last);
+    }
+    
+    return count($results)+1;
 
+}
+
+function calcSumPower($nr){
+    $temp = str_split($nr);
+    $sum = 0;
+    foreach($temp as $val){
+        $sum += pow($val, 2);
+    }
+
+    return $sum;
+}
+
+
+var_dump(squareDigitsSequence(16));
